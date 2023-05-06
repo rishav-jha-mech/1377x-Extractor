9c4b6fc8ec0d740abbf00a68273deab852d38849
rishav-jha-mech <rishav.rj74@gmail.com>
2023-05-07 02:23:03 +0530

    Added select_file function

--- utils.py
+++ utils.py
@@ -34,3 +34,28 @@ def checkAndCreateFolder(folderName):
         os.mkdir(folderName)
     except Exception as e:
         pass
+
+
+def select_file():
+    file_list = os.listdir(OUTPUT_FOLDER)
+    if not file_list:
+        print("No files found in the outputs directory.")
+        return None
+
+    print("Files in the output directory:")
+    for i, filename in enumerate(file_list):
+        print(f"{i+1}. {filename}")
+
+    while True:
+        selection = input("Enter the number of the file you want to select (or 'q' to quit): ")
+        if selection.lower() == "q":
+            return None
+
+        try:
+            index = int(selection) - 1
+            if 0 <= index < len(file_list):
+                return os.path.join(OUTPUT_FOLDER, file_list[index])
+            else:
+                print("Invalid file number. Please try again.")
+        except ValueError:
+            print("Invalid input. Please enter a valid file number.")
