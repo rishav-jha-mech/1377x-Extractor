84522ef6a0a8b0cbeb79a76dc2c603b0e7b33c1a
rishav-jha-mech <rishav.rj74@gmail.com>
2023-05-06 14:50:06 +0530

    Magnet snatcher done

---
+++ magnetSnatcher.py
@@ -0,0 +1,53 @@
+import json
+import time
+import requests
+from bs4 import BeautifulSoup
+
+
+import os
+import re
+
+def sanitize_filename(filename):
+    """
+    Sanitize a filename by replacing all non-alphanumeric characters with underscores.
+    """
+    return re.sub(r'[^\w\d]+', '_', filename)
+
+response = requests.get('https://www.1377x.to/torrent/{torrentid}/{torerentname}')
+html = response.content
+soup = BeautifulSoup(html, 'html.parser')
+
+try:
+    title = soup.title.string
+    genres = [span.text.strip() for span in soup.find('div', class_='torrent-category').find_all('span')]
+    description = soup.find('p').text.strip()
+    magnet_links = []
+    images = []
+
+    for link in soup.find_all('a', href=True):
+        href_value = link['href']
+        if href_value.startswith('magnet:'):
+            magnet_links.append(href_value)
+
+    for img in soup.find_all('img',class_='img-responsive descrimg'):
+        img_value = img['src']
+        if img_value.startswith('https://'):
+            images.append(img_value)
+
+    results = {
+        'title': title,
+        'img': images,
+        'genres': genres,
+        'description': description,
+        'magnet_links': magnet_links,
+    }
+    
+    with open(f'{sanitize_filename(title)}.json', 'a') as f:
+        f.write('[\n')
+        json.dump(results, f)
+        f.write('\n]')
+
+except Exception as e:
+    print('Error on page ...', x, e)
+    with open(f'error-{i}-{x}.html', 'a') as f:
+        f.write(soup.prettify())
